b65c3eee8ed926216d5c083fba32c8b5f5ac1c4a
Washikokevv27 <washikokevv27@gmail.com>
2020-10-04 18:00:44 +0300

    oversigt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Quote } from './quote';
-import { Quote } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +8,11 @@ import { Quote } from '@angular/compiler';
 })
 export class AppComponent {
   quotes:Quote[] = [
-    {id:1, name:'All our dreams can come true, if we have the courage to pursue them'},
-    {id:2,name:'The secret of getting ahead is getting started'},
-    {id:3,name:'Don’t limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve'},
-    {id:4,name:'The best time to plant a tree was 20 years ago. The second best time is now'},
-    {id:5,name:'Only the paranoid survive'},
-    {id:6,name:'It’s hard to beat a person who never gives up'},
+    //{id:1, name:'All our dreams can come true, if we have the courage to pursue them'},
+    //{id:2,name:'The secret of getting ahead is getting started'},
+    //{id:3,name:'Don’t limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve'},
+    //{id:4,name:'The best time to plant a tree was 20 years ago. The second best time is now'},
+    //{id:5,name:'Only the paranoid survive'},
+    //{id:6,name:'It’s hard to beat a person who never gives up'},
   ];
 }
